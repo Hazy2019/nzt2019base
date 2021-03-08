0eeed20fa1fc592b88167dab886513d2a6c8182f
fatiao <me2019@aliyun.com>
2021-03-08 11:52:46 +0800

    update

---
+++ content/posts/db_some-details-about-rocksdb::LRUCache.md
@@ -0,0 +1,11 @@
+---
+title: "some details about rocksdb::LRUCache"
+date: 2021-03-08
+categories:
+ - "db"
+tags:
+ - "lrucache"
+ - "new expression"
+pager: true
+sidebar: "right"
+---
--- scripts/add-new-post.sh
+++ scripts/add-new-post.sh
@@ -4,14 +4,37 @@ function usage()
   echo $0 "<title>" "category" "tag1" "tag2" ...
 }
 
-function output()
+function generate_post_header()
 {
-  if [[ $# < 3 ]]; then
-    usage()
-  fi
-  tl=$1
-  cg=$2
-  for i in {3..$#}; do
-    echo $i
+  title=$1
+  category=$2
+  args=("$@")
+  datestr=$(date '+%Y-%m-%d')
+  
+  echo "---"
+  echo "title: \"$title\""
+  echo "date: $datestr"
+  echo "categories:"
+  echo " - \"$category\""
+  echo "tags:"
+  for ((i=2;i<$#;i++)); do
+    echo " - \"${args[${i}]}\""
   done
+  echo "pager: true"
+  echo "sidebar: \"right\""
+  echo "---"
 }
+
+function output() 
+{
+  if [[ $# -lt 3 ]]; then
+    usage
+    exit -1
+  fi
+  titlesub=${1// /-}
+  filename="content/posts/$2_${titlesub}.md"
+  echo "generate post file: $filename"
+  generate_post_header "$@" | tee $filename
+}
+
+output "$@"
